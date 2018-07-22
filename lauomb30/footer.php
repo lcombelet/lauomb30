7d25533b4d6443a4e559a7436edceeb4d11cd2ff
lcombelet <lc.ombelet@gmail.com>
2018-07-22 14:50:00 +0200

    icons

--- lauomb30/footer.php
+++ lauomb30/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
   <h2>Footer</h2>
-  <p> Copyright &copy; 2000-<?php echo date('Y'); ?> L. Ombelet. All rights reserved.</p>
+  <p> Copyright <i class="far fa-copyright"></i> 2000-<?php echo date('Y'); ?> L. Ombelet. All rights reserved.</p>
 </div>
