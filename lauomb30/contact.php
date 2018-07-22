3a0348888f99da5d5a2db59f10fd001527fe4d90
lcombelet <lc.ombelet@gmail.com>
2018-07-22 22:40:24 +0200

    label

---
+++ lauomb30/contact.php
@@ -0,0 +1,48 @@
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include 'head.php';?>
+</head>
+<body>
+
+<?php include 'header.php';?>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>WELCOME</h2>
+      <h5>A note from the nerd, Jul 10, 2018</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+    <div class="card">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+  <div class="rightcolumn">
+    <?php include 'aboutme.php';?>
+    <?php include 'popular.php';?>
+    <?php include 'social.php';?>
+  </div>
+</div>
+
+<?php include 'footer.php';?>
+
+</body>
+</html>
--- lauomb30/footer.php
+++ lauomb30/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
   <p> Copyright <i class="far fa-copyright"></i> 2000-<?php echo date('Y'); ?>.</p>
-  <p>L. Ombelet. All rights reserved. <a href="contact.php"><i class="far fa-envelope"></i> Contact me</a></p>
+  <p>L. Ombelet. All rights reserved. <a href="contact.php" title="Contact me"><i class="far fa-envelope"></i></a></p>
 </div>
