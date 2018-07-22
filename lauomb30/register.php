a9e5e4d874da832e42878f4660f55ae6e4631c3d
lcombelet <lc.ombelet@gmail.com>
2018-07-22 15:27:16 +0200

    head include

--- lauomb30/register.php
+++ lauomb30/register.php
@@ -109,9 +109,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="/assets/stylesheet.css">
-<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Karla">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+  <?php include 'head.php';?>
 </head>
 <body>
 
