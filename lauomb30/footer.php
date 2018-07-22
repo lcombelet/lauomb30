640194d52dc7c2d62c20f2438f9512c1151de2a0
lcombelet <lc.ombelet@gmail.com>
2018-07-22 14:45:23 +0200

    footer icon reset

--- lauomb30/footer.php
+++ lauomb30/footer.php
@@ -1,4 +1,4 @@
 <div class="footer">
   <h2>Footer</h2>
-  <p> Copyright &copy 2000-<?php echo date('Y'); ?> L. Ombelet. All rights reserved.</p>
+  <p> Copyright &copy; 2000-<?php echo date('Y'); ?> L. Ombelet. All rights reserved.</p>
 </div>
