08779d5f240db4c2789118a802e024b9a48f94eb
lcombelet <lc.ombelet@gmail.com>
2018-07-30 22:48:51 +0200

    update table name

--- lauomb30/register.php
+++ lauomb30/register.php
@@ -14,7 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username_err = "Please enter a username.";
     } else{
         // Prepare a select statement
-        $sql = "SELECT id FROM tbl_users WHERE username = ?";
+        $sql = "SELECT user_id FROM tbl_users WHERE username = ?";
 
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
