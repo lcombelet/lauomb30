809c8e2c4dbc17af34d3954f0ed24624d54fb9e4
lcombelet <lc.ombelet@gmail.com>
2019-05-07 13:12:06 +0200

    yearplan

---
+++ lauomb30/financeyearplan.php
@@ -0,0 +1,43 @@
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'] || in_array("6", $_SESSION['authorizations']))){
+	header("location: login.php");
+	exit;
+}
+
+// Include config file
+require_once 'config.php';
+
+// Define variables and initialize with empty values
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php $title = "LauOmb Webserver - Personal finances";
+  include 'head.php'; ?>
+</head>
+<body>
+
+<?php include 'header.php';?>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Year overview</h2>
+      <h5>Useless bit of text here..</h5>
+      <p>More to come here in the future.</p>
+    </div>
+  </div>
+<div class="rightcolumn">
+  <?php include 'financeside.php';?>
+</div>
+</div>
+
+<?php include 'footer.php';?>
+
+</body>
+</html>
