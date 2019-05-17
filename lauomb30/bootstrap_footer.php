6d5aab2597622d6fff3b86b2a0e6bbd2fcc66d01
lcombelet <lc.ombelet@gmail.com>
2019-05-17 18:10:11 +0200

    cleanup

--- lauomb30/bootstrap_footer.php
+++
@@ -1,2 +0,0 @@
-<p class="float-right"><a href="#">Back to top</a></p>
-<p>&copy; 2000-<?php echo date('Y'); ?>. Laurens Ombelet &middot; all rights reserved &middot; <a href="privacy.php">Privacy</a> &middot; <a href="terms.php">Terms</a></p>
